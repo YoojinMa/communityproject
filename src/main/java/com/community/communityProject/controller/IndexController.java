1d60b0d8185f4d30e68b76691429dae0332692dc
YoojinMa <geronimo9595@naver.com>
2022-03-26 00:50:42 +0900

    [Fix] controller

--- src/main/java/com/community/communityProject/controller/IndexController.java
+++ src/main/java/com/community/communityProject/controller/IndexController.java
@@ -1,6 +1,7 @@
-package com.community.communityProject;
+package com.community.communityProject.controller;
 
-import com.community.communityProject.config.auth.SessionUser;
+import com.community.communityProject.config.oauth.LoginUser;
+import com.community.communityProject.config.oauth.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,34 +13,17 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class IndexController {
 
-//    private final PostsService postsService;
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model)
-    {
-//        model.addAttribute("posts",postsService.findAllDesc());
+    public String index(Model model, @LoginUser SessionUser user) {
 
-        // userName을 사용할 수 있게 model에 저장
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName",user.getName());
         }
+
         return "index";
+
     }
-//    // 글 저장
-//    @GetMapping("/posts/save")
-//    public String postsSave(){
-//        return "posts-save";
-//    }
-//
-//    // 글 수정
-//    @GetMapping("/posts/update/{id}")
-//    public String postsUpdate(@PathVariable Long id, Model model){
-//        PostsResponseDto dto = postsService.findById(id);
-//        model.addAttribute("post",dto);
-//
-//        return "posts-update";
-//    }
 
 }
