cffa4e10ead860fa130007c5255c8e508c477b2b
YoojinMa <geronimo9595@naver.com>
2022-03-26 00:51:52 +0900

    [Fix] repository

--- src/main/java/com/community/communityProject/repository/UserRepository.java
+++ src/main/java/com/community/communityProject/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.community.communityProject.repository;
 import com.community.communityProject.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
